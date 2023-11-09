a0f3afac9e0f394258f80a949b0c68ce4a6afdef
FelipeCardoso559 <111467751+FelipeCardoso559@users.noreply.github.com>
2023-11-09 20:37:19 -0300

    Update index.blade.php

--- resources/views/TemplateAdmin/index.blade.php
+++ resources/views/TemplateAdmin/index.blade.php
@@ -55,19 +55,6 @@
                 <div class="sidebar-brand-text mx-3">Doctor Store</div>
             </a>
 
-            <!-- Divider -->
-            <hr class="sidebar-divider my-0">
-
-            <!-- Nav Item - Dashboard -->
-            <li class="nav-item">
-                <a class="nav-link" href="index.html">
-                    <i class="fas fa-fw fa-tachometer-alt"></i>
-                    <span>Clientes</span></a>
-            </li>
-
-            <!-- Divider -->
-            <hr class="sidebar-divider">
-
             <!-- Heading -->
             <div class="sidebar-heading">
                 Produtos
